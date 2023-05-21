0bc09eb928396eccaacefe43c2c9a874d689253f
modecode22 <epiccoder00@gmail.com>
2023-05-21 10:25:46 +0100

    my starter

--- App.tsx
+++ App.tsx
@@ -3,18 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View  className="flex-1 items-center justify-center bg-zinc-900 text-white">
+      <Text className="text-stone-900 bg-yellow-500 p-1 rounded-xl active:bg-yellow-500/50" >Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
