2d65a9d01c442796b1fc4cc61a72f279735896eb
modecode22 <epiccoder00@gmail.com>
2023-05-22 23:16:55 +0100

    some things

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { Text, SafeAreaView } from 'react-native'
-
+import { Text, SafeAreaView, View } from 'react-native';
 const HomeScreen = () => {
   return (
     <SafeAreaView className='bg-slate-50 text-slate-950 w-full h-full flex justify-center items-center'>
       <Text>Home Screen</Text>
+      <View className='bg-slate-500 rounded-lg p-5 active:bg-black'>
+        <Text>
+        haha
+        </Text>
+      </View>
     </SafeAreaView>
     )
 }
